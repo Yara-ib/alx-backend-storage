822a99246072080924df4d5ce027d7e7317bfead
Yara-ib <yr.ap.23@gmail.com>
2024-07-11 01:54:03 +0300

    Enumeration Added

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,8 +1,8 @@
 -- SQL script that creates a table named (users) with further details; enumeration of countries
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    name VARCHAR(255),
-    country ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
-    UNIQUE(email)
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255),
+    `country` ENUM ('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    UNIQUE(`email`)
 );
