7a30d25ad80ed3289d34763a060072f8d3adae54
Yara-ib <yr.ap.23@gmail.com>
2024-07-17 05:37:56 +0300

    Log Stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,6 +5,7 @@ from pymongo import MongoClient
 
 client = MongoClient('mongodb://127.0.0.1:27017')
 log_collection = client.logs.nginx
+get_count = log_collection.count_documents({"path": "/status"})
 
 
 def find_nd_count(method):
@@ -20,11 +21,12 @@ def find_nd_count(method):
     return log_collection.count_documents(method)
 
 
-print(f'{find_nd_count({})} logs')
-print("Methods:")
-print(f'\t method GET: {find_nd_count({"method": "GET"})}')
-print(f'\t method POST: {find_nd_count({"method": "POST"})}')
-print(f'\t method PUT: {find_nd_count({"method": "PUT"})}')
-print(f'\t method PATCH: {find_nd_count({"method": "PATCH"})}')
-print(f'\t method GET: {find_nd_count({"method": "DELETE"})}')
-print(f'{log_collection.count_documents({"path": "/status"})} status check')
+if __name__ == "__main__":
+    print(f'{find_nd_count({})} logs')
+    print("Methods:")
+    print(f'\t method GET: {find_nd_count({"method": "GET"})}')
+    print(f'\t method POST: {find_nd_count({"method": "POST"})}')
+    print(f'\t method PUT: {find_nd_count({"method": "PUT"})}')
+    print(f'\t method PATCH: {find_nd_count({"method": "PATCH"})}')
+    print(f'\t method GET: {find_nd_count({"method": "DELETE"})}')
+    print(f'{get_count} status check')
