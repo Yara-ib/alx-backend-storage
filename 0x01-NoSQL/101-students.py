95dbe3f07ff817251f50e8cf555b588822a52ebe
Yara-ib <yr.ap.23@gmail.com>
2024-07-17 05:57:36 +0300

    Sorted Scores

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+""" Sort students by average score in Python """
+
+
+def top_students(mongo_collection):
+    """
+    Sort students by average score
+
+    Args:
+        mongo_collection (_type_): collection
+
+    Returns:
+        list: students sorted by average score
+    """
+    return mongo_collection.aggregate(
+        [
+            {
+                "$group": {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"},
+                }
+            },
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
