62ee0e2cf20a51f6f24a412de653745ff1c278e8
Yara-ib <yr.ap.23@gmail.com>
2024-07-17 04:22:25 +0300

    Update Doc | PyMongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" Update a Document in Python """
+
+
+def update_topics(mongo_collection: list, name: str, topics: list) -> None:
+    """
+    Changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection (list): collection object
+        name (str): school name to update
+        topics (list): list of topics approached in the school
+    """
+    mongo_collection.update_one({'name': name}, {'$set': {'topics': topics}})
